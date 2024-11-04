41fce80195d9fb9a6370e488b35a00742dcbad08
amirhoseinzarei <amirhoseinzarei8484@gmail.com>
2024-11-04 23:40:20 +0330

    Add files via upload
    تمرین 3 برسی عدد فیوناچی با c

---
+++ تمرین 3 فیوناچی با سی شارپ/تمرین 3 فیوناچی با سی شارپ/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("لطفاً یک عدد وارد کنید: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (IsFibonacci(n))
+        {
+            Console.WriteLine($"{n} یک عدد فیبوناچی است.");
+        }
+        else
+        {
+            Console.WriteLine($"{n} یک عدد فیبوناچی نیست.");
+        }
+    }
+
+    static bool IsFibonacci(int num)
+    {
+        double sqrt_75 = Math.Sqrt(75);
+        double x = (1 + sqrt_75) / 2;
+        double y = (1 - sqrt_75) / 2;
+
+        return Math.Abs((Math.Pow(x, 2) - Math.Pow(y, 2)) / 2 - num) < 0.00001 &&
+               Math.Abs((Math.Pow(x, 2) + Math.Pow(y, 2)) / 2 - num) < 0.00001;
+    }
+}
